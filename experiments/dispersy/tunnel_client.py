18d13f79b3b511b2d510484d9b9668bb17c283eb
Egbert Bouman <ebouman@gmail.com>
2014-12-05 14:47:42 +0100

    Start Tribler session during tunnel experiment

--- experiments/dispersy/tunnel_client.py
+++ experiments/dispersy/tunnel_client.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-from os import path
+from os import path, getpid
 from random import choice, randint
 from string import letters
 from sys import path as pythonpath
-from time import time
+from time import time, sleep
 
 from twisted.internet.task import LoopingCall
 from twisted.python.log import msg
 
 from gumby.experiments.dispersyclient import DispersyExperimentScriptClient, main
 
-
 # TODO(emilon): Fix this crap
-pythonpath.append(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', "./tribler")))
+BASE_DIR = path.abspath(path.join(path.dirname(__file__), '..', '..', '..'))
+pythonpath.append(path.abspath(path.join(BASE_DIR, "./tribler")))
 
 
 class TunnelClient(DispersyExperimentScriptClient):
@@ -23,12 +23,34 @@ class TunnelClient(DispersyExperimentScriptClient):
         DispersyExperimentScriptClient.__init__(self, *argv, **kwargs)
         self.community_class = TunnelCommunity
 
-        tunnel_settings = TunnelSettings()
-        tunnel_settings.socks_listen_ports = [randint(1000, 65535) for _ in range(5)]
-        self.set_community_kwarg('settings', tunnel_settings)
+        self.session = self.start_tribler()
+        self.session.set_anon_proxy_settings(2, ("127.0.0.1", self.session.get_tunnel_community_socks5_listen_ports()))
+        self.set_community_kwarg('session', self.session)
 
         self.monitor_circuits_lc = None
         self._prev_scenario_statistics = {}
+
+    def start_tribler(self):
+        from Tribler.Core.SessionConfig import SessionStartupConfig
+        from Tribler.Core.Session import Session
+        config = SessionStartupConfig()
+        config.set_install_dir(path.abspath(path.join(BASE_DIR, "./tribler")))
+        config.set_state_dir(path.abspath(path.join(BASE_DIR, ".Tribler-%d") % getpid()))
+        config.set_torrent_checking(False)
+        config.set_multicast_local_peer_discovery(False)
+        config.set_megacache(False)
+        config.set_dispersy(False)
+        config.set_mainline_dht(True)
+        config.set_torrent_collecting(False)
+        config.set_libtorrent(True)
+        config.set_dht_torrent_collecting(False)
+        config.set_videoplayer(False)
+        session = Session(config)
+        upgrader = session.prestart(u":memory:")
+        while not upgrader.is_done:
+            sleep(0.1)
+        session.start()
+        return session
 
     def registerCallbacks(self):
         self.scenario_runner.register(self.build_circuits, 'build_circuits')
@@ -36,6 +58,11 @@ class TunnelClient(DispersyExperimentScriptClient):
     def build_circuits(self):
         msg("build_circuits")
         self._community.circuits_needed[3] = 8
+
+    def start_dispersy(self, autoload_discovery=True):
+        DispersyExperimentScriptClient.start_dispersy(self, autoload_discovery)
+        self.session.lm.dispersy = self._dispersy
+        self.session.set_dispersy_port(self._dispersy.endpoint._port)
 
     def online(self):
         DispersyExperimentScriptClient.online(self)
